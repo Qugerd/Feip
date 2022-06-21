84dfd18ae01cd0cd91720bc65ff7e3fcd038ca93
Qugerd <qugerd.main@gmail.com>
2022-06-21 17:07:10 +1000

    hw2

--- app/src/main/java/com/example/fitnesstracker/Data/DataActivityUsers.kt
+++ app/src/main/java/com/example/fitnesstracker/Data/DataActivityUsers.kt
@@ -14,8 +14,7 @@ class DataActivityUsers {
         ListItem.Date(2, DateTime.now().toString(dateFormat)),
         ListItem.Card(3 , "@она", "серфинг", "1 км", "3 мин", DateTime.now().toString(dateFormat)),
         ListItem.Date(4, DateTime.now().toString(dateFormat)),
-        ListItem.Card(5 , "@Путин", "флекс", "12 км", "23 мин", DateTime.now().toString(dateFormat)),ListItem.Date(0, DateTime.now().toString(dateFormat)),
-        ListItem.Card(1 , "@он", "Бег", "322 км", "10 мин", DateTime.now().toString(dateFormat)),
+        ListItem.Card(5 , "@Путин", "флекс", "12 км", "23 мин", DateTime.now().toString(dateFormat))
     )
 
     fun getData() = data
